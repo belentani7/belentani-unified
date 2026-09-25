14d469cee956615e2c619db8a26b3564f70563eb
Belentani <belentani7pedro@gmail.com>
2026-09-25 14:41:36 +0200

    chore: sincronizacion y calidad de repo

--- apps/manos-abiertas/src/app/[locale]/(sections)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from 'react';
-
-interface SectionsLayoutProps {
-  children: ReactNode;
-}
-
-export default function SectionsLayout({ children }: SectionsLayoutProps) {
-  return <main id="main-content">{children}</main>;
-}
